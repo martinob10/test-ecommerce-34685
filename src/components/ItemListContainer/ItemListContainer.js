11f04434549b6749b78ba6aa4876d3a13a489f9f
Christian Daniel Ortiz Orobia <cortiz@sms-sudamerica.com>
2022-07-19 20:59:43 -0300

    Clase 5 | Componentes II

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,13 +1,36 @@
+import { useEffect } from "react"
 import ItemProduct from "../ItemProduct/ItemProduct"
 import './ItemListContainer.scss'
 
 const ItemListContainer = ({section}) => {
+    const product1 = {
+        title: "Campera Invierno",
+        price: 15000,
+        image: 'img1.webp',
+        stock: 6
+    }
+
+    const product2 = {
+        title: "Remera",
+        price: 34000,
+        image: 'img2.webp',
+        stock: 10
+    }
+
+    useEffect( () => {
+        //console.log("Ejecuto en fase de actualizacion")
+        // fetch().then(
+        //     setProduct()
+        // )
+    })
+
+    const handleSubmit = () => {console.log("Envio de info")}
 
     return(
         <div className='list-products'>
             <h2>{section}</h2>
-            <ItemProduct title="Campera Invierno" price={15000} image={'img1.webp'}/>
-            <ItemProduct title="Remera" price={3400} image={'img2.webp'}/>
+            <ItemProduct data={product1} action={handleSubmit}/>
+            <ItemProduct data={product2}/>
         </div>
     )
 }
--- src/components/ItemProduct/ItemProduct.js
+++ src/components/ItemProduct/ItemProduct.js
@@ -1,13 +1,51 @@
+import { useState, useEffect } from 'react'
 import './ItemProduct.scss'
 
-const ItemProduct = ({title, price, image}) => {
+const ItemProduct = ({data, action}) => {
+    const [contador, setContador] = useState(1)
+
+    const {title, image, price, stock} = data
+
+    const addNumber = () => {
+        setContador(contador + 1)
+    }
+    const removeNumber = () => {
+        setContador(contador - 1)
+    }
+    //1. Mount -> Crea
+    // useEffect( () => {
+
+    // }, [])
+
+    //2. Update/Change/Actualizacion -> Cambio el valor State/prop
+    // useEffect( () => {
+
+    // })
+    //3. Unmount/Desmontaje -> Destruye, desaparece
+    // useEffect( () => {
+        // return () => {
+            //     console.log("se desmonta el componente")
+            // }
+    // })
+
+    useEffect( () => {
+        console.log("Actualizacion")
+        // setContador(1)
+    }, [contador])
+
+
 
     return(
         <div className="item-product">
             <img src={`/assets/${image}`} alt="Imagen producto" />
             <p>{title}</p>
             <span>$ {price}</span>
-            <button>Comprar</button>
+            <div className='countProd'>
+                <button onClick={removeNumber}>-</button>
+                <p>{contador}</p>
+                <button onClick={addNumber}>+</button>
+            </div>
+            <button onClick={action}>Comprar</button>
         </div> 
     )
 }
